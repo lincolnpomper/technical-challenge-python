e12c9170538424e51ffd08662f9bf6a0a240f850
Lincoln Pompermaier <lincoln@pompermaier.com>
2020-07-27 13:49:07 -0300

    add python code for REST challenge

---
+++ RequestCache.py
@@ -0,0 +1,71 @@
+from typing import Dict
+import requests
+import redis
+
+
+def smart_cache(func):
+
+    def wrapped(request_cache, url, parameters, *args):
+
+        def get_key():
+
+            parameters_str = ""
+            for param in parameters:
+                parameters_str += param
+            return url + ";" + parameters_str
+
+        key = get_key()
+        stored_json = request_cache.redis_cache.get(key)
+
+        if stored_json is None:
+            json_value = func(request_cache, url, parameters, *args)
+            request_cache.redis_cache.set(key, json_value)
+        else:
+            json_value = stored_json
+
+        return json_value
+    return wrapped
+
+
+def invalidate_cache(func):
+
+    def wrapped(request_cache, url, parameters, *args):
+
+        def get_key():
+
+            parameters_str = ""
+            for param in parameters:
+                parameters_str += param
+            return url + ";" + parameters_str
+
+        key = get_key()
+        request_cache.redis_cache.set(key, None)
+
+        return func(request_cache, url, parameters, *args)
+    return wrapped
+
+
+class RequestCache:
+
+    def __init__(self):
+        self.redis_cache = redis.Redis(host='localhost', port=6379, db=0)
+
+    @smart_cache
+    def get(self, url, parameters=None):
+        return requests.get(url, params=parameters).json()
+
+    @invalidate_cache
+    def post(self, url, parameters=None, data=None):
+        return requests.post(url, params=parameters, data=data).json()
+
+    @invalidate_cache
+    def put(self, url, parameters=None, data=None):
+        return requests.put(url, params=parameters, data=data).json()
+
+    @invalidate_cache
+    def patch(self, url, parameters=None, data=None):
+        return requests.patch(url, params=parameters, data=data).json()
+
+    @invalidate_cache
+    def delete(self, url, parameters=None, data=None):
+        return requests.delete(url, params=parameters, data=data).json()
---
+++ request_test.py
@@ -0,0 +1,63 @@
+import unittest
+from RequestCache import RequestCache
+from unittest.mock import patch
+
+
+class RequestTest(unittest.TestCase):
+    URL = "http://localhost"
+    PARAMETERS = []
+    DATA = [{'key1': 'value1'}]
+    REDIS_CONTENTS = [{'key1': 'value1'}]
+    RESPONSE_CONTENTS = [{'key1': 'value1'}]
+
+    class RedisMock:
+
+        def get(self, key):
+            return RequestTest.REDIS_CONTENTS
+
+        def set(self, key, value):
+            pass
+
+    class RedisMockEmpty:
+
+        def get(self, key):
+            return None
+
+        def set(self, key, value):
+            pass
+
+    class ResponseMock:
+        def json(self):
+            return RequestTest.RESPONSE_CONTENTS
+
+    @patch('requests.get', return_value=ResponseMock())
+    @patch('redis.Redis', return_value=RedisMockEmpty())
+    def test_get_previously_uncached(self, redis_mock, response_mock):
+        self.requestCache = RequestCache()
+        self.assertEqual(self.requestCache.get(RequestTest.URL, RequestTest.PARAMETERS), RequestTest.REDIS_CONTENTS)
+
+        redis_mock.assert_called_once()
+        response_mock.assert_called_once()
+
+    @patch('requests.get', return_value=ResponseMock())
+    @patch('redis.Redis', return_value=RedisMock())
+    def test_get_already_cached(self, redis_mock, response_mock):
+        self.requestCache = RequestCache()
+        self.assertEqual(self.requestCache.get(RequestTest.URL, RequestTest.PARAMETERS), RequestTest.REDIS_CONTENTS)
+
+        redis_mock.assert_called_once()
+        response_mock.assert_not_called()
+
+    @patch('requests.post', return_value=ResponseMock())
+    @patch('redis.Redis', return_value=RedisMock())
+    def test_post(self, redis_mock, response_mock):
+        self.requestCache = RequestCache()
+        self.assertEqual(self.requestCache.post(RequestTest.URL, RequestTest.PARAMETERS, RequestTest.DATA),
+                         RequestTest.RESPONSE_CONTENTS)
+
+        redis_mock.assert_called_once()
+        response_mock.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
